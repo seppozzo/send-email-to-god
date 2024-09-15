8c94c29fe81b2bb54df958168ec03f04ac1d4385
seppozzo <giusevinci207@gmail.com>
2024-09-15 20:02:57 +0200

    Add files via upload

---
+++ send-mail.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>send mail</title>
+</head>
+<style>
+
+.feedback
+{
+text-align:center;
+font-size:50pt;
+padding-top: 300px;
+}
+
+
+    </style>
+<body>
+
+<div class="feedback">
+
+<?php
+//message
+$msg=$_POST["Message"];
+
+print ("Email inviata! Riceverai un messaggio di conferma alla ricezione di Cristo.");
+?>
+</div>
+</body>
+</html>
+
